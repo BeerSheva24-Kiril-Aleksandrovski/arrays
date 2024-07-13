890fd9a3e30978f86d4fbf7805f6003c0ce7dfc7
Kiril <fanky.yahoo@gmail.com>
2024-07-13 20:15:55 +0300

    Methods binarySearch, insertSorted, isOneSwap were added. And tests for them

--- src/main/java/telran/util/Arrays.java
+++ src/main/java/telran/util/Arrays.java
@@ -14,19 +14,105 @@ public class Arrays {
         res[ar.length] = number;
         return res;
     }
-    
+
     public static int[] insert(int[] ar, int index, int number) {
-        int [] newArr = new int[ar.length + 1];
-        System.arraycopy(ar, 0, newArr, 0, index);
-        newArr [index] = number;
+        int[] newArr = java.util.Arrays.copyOf(ar, ar.length + 1);
+        newArr[index] = number;
         System.arraycopy(ar, index, newArr, index + 1, ar.length - index);
         return newArr;
     }
 
     public static int[] remove(int[] numbers, int index) {
-        int [] newArr = new int[numbers.length - 1];
-        System.arraycopy(numbers, 0, newArr, 0, index);
-        System.arraycopy(numbers, index + 1, newArr, index, numbers.length - index - 1);
+        int[] newArr = java.util.Arrays.copyOf(numbers, numbers.length - 1);
+        System.arraycopy(numbers, index + 1, newArr, index, newArr.length - index);
         return newArr;
+    }
+
+    private static boolean pushMaxAtEnd(int[] ar, int length) {
+        boolean res = true;
+        for (int i = 0; i < length; i++) {
+            if (ar[i] > ar[i + 1]) {
+                res = false;
+                swap(ar, i, i + 1);
+            }
+        }
+        return res;
+    }
+
+    public static void swap(int[] ar, int i, int j) {
+        int tmp = ar[i];
+        ar[i] = ar[j];
+        ar[j] = tmp;
+    }
+
+    public static int[] sort(int[] ar) {
+        int length = ar.length;
+        boolean flSorted = false;
+        while (!flSorted) {
+            length--;
+            flSorted = pushMaxAtEnd(ar, length);
+        }
+        return ar;
+    }
+
+    /**
+     * 
+     * @param ar  - sorted array
+     * @param key - being searched number
+     * @return see comments definition
+     */
+    public static int binarySearch(int[] ar, int key) {
+        // TODO
+        // index of the search key, if it is contained in the array;
+        // otherwise, (-(insertion point) - 1).
+        // The insertion point is defined as the point at which the key would be
+        // inserted into
+        // the array: the index of the first element greater than the key, or a.length
+        // if all elements in the array are less than the specified key. Note that this
+        // guarantees that the return value will be >= 0 if and only if the key is
+        // found.
+        int low = 0, high = ar.length - 1;
+        int res = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (ar[mid] < key) {
+                low = mid + 1;
+            } else if (ar[mid] > key) {
+                high = mid - 1;
+            } else if (ar[mid] == key) {
+                res = mid;
+                break;
+            }
+        }
+        return res == -1 ? -low - 1 : res;
+    }
+
+    public static int[] insertSorted(int[] arSorted, int number) {
+        // TODO
+        // arSorted is sorted array
+        // to insert number at index to keep the array sorted
+        // additional sorting is disallowed
+        int[] newArr = java.util.Arrays.copyOf(arSorted, arSorted.length + 1);
+        int pos = binarySearch(arSorted, number);
+        if (pos < 0) {
+            pos = Math.abs(pos) - 1;
+            newArr[pos] = number;
+            System.arraycopy(arSorted, pos, newArr, pos + 1, arSorted.length - pos);
+        }
+        return newArr;
+    }
+
+    public static boolean isOneSwap(int[] array) {
+        ////TODO
+        //return true if a given array has exactly one swap to get sorted array
+        //the swaped array's elements may or may not be neighbors 
+        int[] newArr = java.util.Arrays.copyOf(array, array.length);
+        int[] sortedArr = sort(newArr);
+        int mismatch = 0;
+        for (int i = 0; i < array.length; i++) {        //swaping is a action with two elements
+            if (array[i] != sortedArr[i])               //so if we have 2 elements on wrong places => isOneSwap must return true
+                mismatch++;                             //if we have only 1 element(have no pair for swaping) => isOneSwap must return false
+        }                                               //if we have sorted array and no elements on a wrong places => isOneSwap must return false
+        return mismatch == 2 ? true : false;            
     }
 }
--- src/test/java/telran/util/test/ArraysTest.java
+++ src/test/java/telran/util/test/ArraysTest.java
@@ -2,10 +2,15 @@ package telran.util.test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import static telran.util.Arrays.*;
+
+import java.util.Random;
 public class ArraysTest {
+private static final int N_ELEMENTS = 1_000;
 int[] numbers = {10, 7, 12, -4, 13, 3, 14};
 @Test
 void searchTest(){
@@ -44,6 +49,64 @@ void removeTest() {
     assertArrayEquals(expected2, remove(numbers,6));
     int [] expected3 = {10, 7, -4, 13, 3, 14};
     assertArrayEquals(expected3, remove(numbers,2));
+
+}
+@Test
+void sortTest () {
+    int [] testNubers = java.util.Arrays.copyOf(numbers, numbers.length);
+    int [] expected = {-4, 3, 7, 10, 12, 13, 14};
+    sort(testNubers);
+    assertArrayEquals(expected, testNubers);
+}
+@Test
+void sortTestRandomArray () {
+    int [] array = getRandomlyArray (N_ELEMENTS);
+    int limit = array.length - 1;
+    sort(array);
+    for (int i = 0; i < limit; i++){
+        assertTrue(array [i] <= array [i + 1]);
+    }
+}
+
+private int[] getRandomlyArray(int nElements) {
+    int [] res = new int[nElements];
+    Random random = new Random();
+    for (int i = 0; i < nElements; i++){
+        res [i] = random.nextInt();
+    }
+    return res;
+}
+@Test
+void binarySearchTest () {
+    int [] expected =  {-4, 3, 7, 10, 12, 13, 14};
+    assertEquals(3, binarySearch(expected, 10));
+    assertEquals(0, binarySearch(expected, -4));
+    assertEquals(6, binarySearch(expected, 14));
+    assertEquals(-8, binarySearch(expected, 15));
+    assertEquals(-1, binarySearch(expected, -5));
+}
+@Test
+void insertSortedTest () {
+    int [] arSorted =  {-4, 3, 7, 10, 12, 13, 14};
+    int [] expectedBeg =  {-5, -4, 3, 7, 10, 12, 13, 14};
+    assertArrayEquals(expectedBeg, insertSorted(arSorted, -5));
+    int [] expectedEnd =  {-4, 3, 7, 10, 12, 13, 14, 15};
+    assertArrayEquals((expectedEnd), insertSorted(arSorted, 15));
+    int [] expectedMid =  {-4, 3, 4, 7, 10, 12, 13, 14};
+    assertArrayEquals(expectedMid, insertSorted(arSorted, 4));
+    int [] expectedMid1 =  {-4, 3, 7, 10, 11, 12, 13, 14};
+    assertArrayEquals(expectedMid1,insertSorted(arSorted, 11));
+}
+@Test
+void isOneSwapTest() {
+    assertTrue(isOneSwap(new int [] {1,5,3,4,2,5}));        //need to swap 5 and 2 => number of swaps = 1
+    assertFalse(isOneSwap(new int [] {1,2,3,4,5,6,1,4}));   //number of swaps > 1
+    assertTrue(isOneSwap(new int [] {1,6,3,4,5,2,6}));      //need to swap 6 and 2 => number of swaps = 1
+    assertTrue(isOneSwap(new int [] {7,2,3,3,3,4,5,6,1}));  //need to swap 7 and 1 => number of swaps = 1
+    assertFalse(isOneSwap(new int [] {1,2,3,4,5,6}));       //no need to swap
+    assertTrue(isOneSwap(new int [] {1,2,4,3,5,6}));        //swap 4 and 3 => number of swaps = 1 
+    assertFalse(isOneSwap(new int [] {1,2,3,5,6,4}));       //numer of swops = 2
+
 
 }
 }
